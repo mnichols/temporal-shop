96ef1e640885677d35c523a6138a1ed88a15b74c
Mike Nichols <mike.nichols@temporal.io>
2022-04-27 00:24:03 -0400

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import '../styles/globals.css';
+import store from '../redux/store';           // Importing redux store
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { Provider } from 'react-redux';       // Importing Provider
+
+//
+// function MyApp({ Component, pageProps }: AppProps) {
+//   return <Component {...pageProps} />
+// }
+
+function MyApp({ Component, pageProps }) {
+  return (
+      <Provider store={store}>
+          <div className={"wrapper"}>
+            <Navbar />
+            <Component {...pageProps} />
+            <Footer />
+          </div>
+      </Provider>
+  );
 }
-
 export default MyApp
---
+++ pages/shop.tsx
@@ -0,0 +1,18 @@
+import ProductCard from '../components/ProductCard';
+import styles from '../styles/ShopPage.module.css';
+import { getProducts } from './api/products/index';
+
+const ShopPage = ({ products }) => {
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>All Results</h1>
+            <div className={styles.cards}>
+                {products.map((product) => (
+                    <ProductCard key={product.id} product={product} />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default ShopPage;
