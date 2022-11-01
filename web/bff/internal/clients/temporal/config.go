5a6b9beeae66a4e87f132e191b88a9f992da45b4
Mike Nichols <mike.nichols@temporal.io>
2022-11-01 15:02:44 -0400

    fix docker

--- web/bff/internal/clients/temporal/config.go
+++ web/bff/internal/clients/temporal/config.go
@@ -1,8 +1,10 @@
 package temporal
 
 type Config struct {
-	HostPort  string
-	Namespace string
+	HostPort     string
+	Namespace    string
+	CloudCertPem string
+	CloudCertKey string
 }
 
 func (c *Config) Prefix() string {
